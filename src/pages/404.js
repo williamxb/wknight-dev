ea7ca1588e9e6cfc698d4ef899b64fb080b7e2f3
Will <will.k.01@live.co.uk>
2021-01-14 18:10:06 +0000

    initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import { div } from "prelude-ls"
+import React from "react"
+
+const NotFoundPage = () => (
+    <div>
+        <h1>404 - Page not found</h1>
+    </div>
+)
+
+export default NotFoundPage
