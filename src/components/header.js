60161aa1a8db3f7f082287ee7ac84e6f2725f347
Will <will.k.01@live.co.uk>
2021-01-17 14:35:40 +0000

    Added homepage

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,27 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Header = () => (
-  <header>
-    <p>header</p>
-  </header>
+    <header>
+        <div className="container">
+            <div className="inner-header">
+                <div className="logo">
+                    <Link to="/">THE CROWN INN</Link>
+                </div>
+                <div className="navigation">
+                    <nav>
+                        <Link to="/restaurant">About</Link>
+                        <Link to="/bar">Bar</Link>
+                        <Link to="/rooms">Rooms</Link>
+                        <Link to="/events">Events</Link>
+                        <Link to="/the-local-area">The Local Area</Link>
+                        <Link to="/contact-us">Contact Us</Link>
+                        <Link to="/press">Press</Link>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
 )
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
-//scss
+// scss
 import "../styles/styles.scss"
 // Normalize is optional up to you
 import "normalize.css"
 
+// Import components
+import Header from "../components/header"
+import hero from "../images/hero.jpg"
+
+
 const IndexPage = () => (
-  <div>
-    <h1>Hi from the home page</h1>
-    <Link to="/page-2">Go to page 2</Link>
-  </div>
+    <div>
+        <Header />
+        <div className="hero-container">
+            <img className="hero-image" src={hero} alt="hero image"></img>
+            <div className="hero-content-outer">
+                <h1>
+                    <span>The</span>Crown Inn<span>Church Enstone</span>
+                </h1>
+                <p>
+                    A hidden gem located in the rural Oxfordshire village of
+                    Church Enstone, the perfect spot for any occasion from a
+                    spontaneous lunch to celebratory dinner, in either our flag
+                    stoned bar area, restaurant, conservatory, or weather
+                    permitting our secluded garden area.
+                </p>
+            </div>
+        </div>
+    </div>
 )
 
 export default IndexPage
