07040c14f37e45d6cb245e47bd50cf264fc841f9
Will <will.k.01@live.co.uk>
2021-01-18 13:48:24 +0000

    Updated dependancies, fixed header

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,8 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
